81e5d69dababaf3ac012e62b9c7ace7253e66f5e
Malavika Vasist <47785763+MalAstronomy@users.noreply.github.com>
2020-02-05 13:29:24 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,17 @@
+from VIP_test import Dataset
+from VIP_test import Preproc
+from VIP_test import Algorithm
+from VIP_test import Evaluation
+
+Dataset= Dataset()
+Dataset.printing_name()
+
+Preproc=Preproc()
+Preproc.printing_name()
+
+Algorithm=Algorithm()
+Algorithm.printing_name()
+
+Evaluation=Evaluation()
+Evaluation.printing_name()
+
